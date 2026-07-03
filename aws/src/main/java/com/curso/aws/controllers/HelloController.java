5b1b3d4241f3cecf519f53803d1067ee44215b3c
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2026-07-02 21:14:02 -0300

    Update HelloController.java

--- aws/src/main/java/com/curso/aws/controllers/HelloController.java
+++ aws/src/main/java/com/curso/aws/controllers/HelloController.java
@@ -1,14 +1,14 @@
 package com.curso.aws.controllers;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World!";
+        return ResponseEntity.ok("Funcionando!");
     }
 }
